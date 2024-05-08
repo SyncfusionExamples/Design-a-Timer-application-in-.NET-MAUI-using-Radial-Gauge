15c161f02a00f0bb3de5beccce4b94437d3c1670
Jeyasri Murugan <jeyasrim@syncfusion.com>
2024-05-08 17:51:13 +0530

    Window size changes added

--- TimerApplication/TimerApplication/App.xaml.cs
+++ TimerApplication/TimerApplication/App.xaml.cs
@@ -1,12 +1,20 @@
-﻿namespace TimerApplication
+﻿
+
+namespace TimerApplication
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
             MainPage = new MainPage();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Height = 600;
+            return window;
         }
     }
 }
